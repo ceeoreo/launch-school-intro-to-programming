4bfe5cacbe9d5a32d770f9568926c0d8e4624227
Ceora Ford <ceoraallthetime@gmail.com>
2018-12-10 21:01:36 -0500

    add variables chapter exercises from Launch School prep

---
+++ Variables/name.rb
@@ -0,0 +1,12 @@
+puts "Tell me your first name"
+first_name = gets.chomp
+
+
+puts "Tell me your last name"
+last_name = gets.chomp
+
+puts "Hello #{first_name} #{last_name}"
+
+
+
+10.times {puts first_name}
