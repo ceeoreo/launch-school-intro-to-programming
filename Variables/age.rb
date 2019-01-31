8d5eaa7439056c076336e7242c4ae3aebb8d9056
Ceora Ford <ceoraallthetime@gmail.com>
2019-01-30 19:11:39 -0500

    made minor changes to age.rb during review

--- Variables/age.rb
+++ Variables/age.rb
@@ -13,3 +13,23 @@ puts "In 30 years you will be #{age}"
 
 age += 10
 puts "In 40 years you will be #{age}"
+
+
+
+=begin
+
+puts "How old are you?"
+
+age = gets.chomp.to_i
+
+puts "In 10 years you will be:"
+print age + 10
+
+puts "In 20 years you will be:"
+print age + 20
+
+puts "In 30 years you will be:"
+print age + 30
+
+puts "In 40 years you will be:"
+pritn age + 40 
